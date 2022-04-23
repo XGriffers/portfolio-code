2e2b2e07e9c94537b2d5a29613a8e4a9871a3ee5
Xgriffers <stevengriffith6@student.purdueglobal.edu>
2022-04-23 19:58:04 -0400

    form Fixes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -58,12 +58,12 @@ export default function Contact(){
               <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                 PHONE
               </h2>
-              <p className="leading-relaxed">8143310196</p>
+              <p className="leading-relaxed">(814) 331-0196</p>
             </div>
           </div>
         </div>
         <form data-netlify="true"
-          name="contactForm"
+          name="contact"
           method="post"
           onSubmit={handleSubmit}
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
@@ -76,40 +76,38 @@ export default function Contact(){
           </p>
 
           <div className="relative mb-4">
-            {/* <label htmlFor="name" className="leading-7 text-sm text-gray-400">
+             <label htmlFor="name" className="leading-7 text-sm text-gray-400">
               Name
-            </label> */}
+            </label> 
             <input
               type="text"
               id="name"
               name= "name"
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              placeholder="Name"
             />
           </div>
           <div className="relative mb-4">
-            {/* <label htmlFor="email" className="leading-7 text-sm text-gray-400">
+            <label htmlFor="email" className="leading-7 text-sm text-gray-400">
               Email
-            </label> */}
+            </label>
             <input
               type="email"
               id="email"
               name="email"
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              placeholder="Email"
-            />
+              />
           </div>
           <div className="relative mb-4">
-            {/* <label
+             <label
               htmlFor="message"
               className="leading-7 text-sm text-gray-400">
               Message
-            </label> */}
+            </label> 
             <textarea
               id="message"
               name="message"
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-              placeholder="Please leave a message..."
+              
             />
           </div>
           <button
