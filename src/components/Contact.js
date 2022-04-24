b487b07376a7f23e812ea2f109854ea1ac08a709
Xgriffers <stevengriffith6@student.purdueglobal.edu>
2022-04-23 20:08:22 -0400

    form Fixes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -20,7 +20,7 @@ export default function Contact(){
         })
         .then(() => alert("Message sent!"))
         .catch((error) => alert(error));
-        e.preventDefault();
+        e.preventDefault(e);
       }
 
       return(
@@ -64,7 +64,7 @@ export default function Contact(){
         </div>
         <form data-netlify="true"
           name="contact"
-          method="post"
+          method="POST"
           onSubmit={handleSubmit}
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
           <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
